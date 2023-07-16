1d03a3f65c678ee856bcbfac25b5414df2445bc8
mike030188 <halol.imnida2023@gmail.com>
2023-07-16 18:17:50 +0500

    feat:Todolist-1 task is done

--- src/components/todoList/TodoList1.jsx
+++ src/components/todoList/TodoList1.jsx
@@ -1,65 +1,84 @@
-import React, { useState } from 'react';
-import {FaTrashAlt} from 'react-icons/fa';
-import { Container, Input, Main, Todos } from './TodoList1.style';
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import { Container, Input, Main, Todos } from "./TodoList1.style";
 
-function toListInfo () {
-    const toDoListText = [];
-    for (let i=0; i < 0; i++) {
-         toDoListText.push({ 
-        id: i,
-        text: 'Item:' + (i + 1),
+function toListInfo() {
+  const toDoListText = [];
+  for (let i = 0; i < 0; i++) {
+    toDoListText.push({
+      id: i,
+      text: "Item:" + (i + 1),
     });
-    }
+  }
 
-   return toDoListText;
-
+  return toDoListText;
 }
 
 const TodoList1 = () => {
-    const [todoList, setTodoList] = useState(toListInfo);
-    const [text, setText] = useState("");
+  const [todoList, setTodoList] = useState(toListInfo);
+  const [text, setText] = useState("");
 
-    const handleChange = (e) => {
-        setText(e.target.value);
-    };
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const deleteTodo = id => {
-        setText(todoList.filter(item => item.id !== id))
-    }
+  const deleteTodo = (id) => {
+    setTodoList(todoList.filter((item) => item.id !== id));
+  };
+
   return (
     <Main>
-        <Container >
-            <h2>Todo list</h2>
-                <Input>                    
-                    <input style={{width:'100%'}} type="text" value={text} onChange={handleChange}/>
-                    <button style={{padding:'5px', background:'lightblue', border:'none'}}
-                        onClick={() => {
-                        setText("");
-                        setTodoList([{
-                            id:todoList.length,
-                            text: text,
-                        }, 
-                        ...todoList
-                    ]);
-                    }}>Add</button>
-                </Input>
-                <Todos>
-                    <p>{todoList.map((item) => (
-                    <div style={{width:'100%'}}
-                    key={item.id}>
-                        {item.text}
-                    <FaTrashAlt onClick={(item)=> deleteTodo(   )} />                       
-                    </div>   
-                    
-                    ))}
-                    </p>
-                    
-                    
-                </Todos>
+      <Container>
+        
+        <Input>
+          <input    
+            autoFocus        
+            type="text"
+            value={text}
+            onChange={handleChange}
+          />
+          <button            
+            onClick={() => {
+              setText("");
+              setTodoList([
+                {
+                  id: todoList.length,
+                  text: text,
+                },
+                ...todoList,
+              ]);
+            }}
+          >
+            Add
+          </button>
+        </Input>
+        <Todos>
+          <p>
+            {todoList.map((item) => (
                 
-            </Container>
+              <div style={{ width: "100%", 
+                    display:'flex', 
+                    justifyContent: 'space-between', 
+                    alignItems:'center',
+                    padding:'10px',
+                    border: '1px solid grey', 
+                    backgroundColor:'#c5e1e5',
+                    borderRadius: '8px',
+                    margin: '12px 0',
+                    fontSize: '20px',
+                    letterSpacing: '2px'
+                }} 
+                    key={item.id}>
+                <div>{item.text}</div>
+                <div style={{padding:'10px 12px', borderRadius:'8px', backgroundColor:'#1c1ca1'}}><FaTrashAlt color="white" onClick={() => deleteTodo(item.id)} /></div>
+                
+              </div>
+            ))}
+          </p>
+          
+        </Todos>
+      </Container>
     </Main>
-    
   );
 };
 
--- src/components/todoList/TodoList1.style.js
+++ src/components/todoList/TodoList1.style.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 
 export const Main = styled.div`
-background-color:green;
+background-color:#2461cc;
 height: 100vh;
 display:flex;
 justify-content: center;
 align-items: center;
-
+font-family: 'Roboto', sans-serif;
 
 `
     
@@ -15,25 +15,51 @@ align-items: center;
 export const Container = styled.div`
 
 background-color: #FFF;
-height: 600px;
-width: 400px;
+height: 490px;
+width: 550px;
 border-radius: 15px;
-padding: 15px;
+padding: 30px 15px;
 
-h2{
-    margin-bottom: 10px;
-}
 
 `
 export const Input = styled.div`
 display:flex;
 width: 100%;
-margin-bottom: 10px;
+margin-bottom: 50px;
+
+input{
+    width: 100%;
+    border:1px solid #2c6beb;
+    margin-right: 30px;    
+    border-radius: 7px;
+    padding: 10px;
+    font-size:18px;
+
+    
+}
+input:focus {
+  border-color: red;
+}
+
+
+
+button{
+    background-color: #2c6beb;
+    width: 140px;
+    padding:15px;
+    border-radius: 7px;
+    border: none;
+    font-size: 20px;
+    color: #fff;
+    font-weight: 600;
+}
 `
 export const Todos = styled.div`
 display:flex;
-overflow-y: scroll;
+/* overflow-y: scroll; */
 width: 100%;
+justify-content: space-between;
+
 
 p{
     
