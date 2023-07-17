484962f74428088ebf74265cb8e3510195faace1
mike030188 <halol.imnida2023@gmail.com>
2023-07-17 19:18:24 +0500

    feat: Input and button is done

--- src/components/todoList/TodoList2.jsx
+++ src/components/todoList/TodoList2.jsx
@@ -1,5 +1,5 @@
 import React  from "react";
-import {Container, Main} from "./TodoList2.style";
+import {Container, Input, Main, Title} from "./TodoList2.style";
 
 
 
@@ -9,13 +9,42 @@ const TodoList2 = () => {
   return (
     <Main>
         <div style={{display:'flex', flexDirection:'column', width:'1000px'}}>
-           <h2>TODO List Demo App</h2>
-            <p style={{margin:"5px 0 80px 0",
+           <h1>TODO List Demo App</h1>
+            <p style={{margin:"5px 0 50px 0",
                 background:'lightyellow', width:'70px'
                 }}>Do it now.</p> 
-        </div>
-        
+        </div>     
+        <Input>
+            <input    
+                      
+                type="text"
+                /* value={text}
+                onChange={handleChange} */
+            />
+            <button            
+               /*  onClick={() => {
+                setText("");
+                setTodoList([
+                    {
+                    id: todoList.length,
+                    text: text,
+                    },
+                    ...todoList,
+                ]);
+                }} */
+            >
+                Add Task
+            </button>
+            </Input>   
+            <Title>
+                <div>#</div>
+                <div>Task Name</div>
+                <div>Status</div>
+                <div>Edit</div>
+                <div>Remove</div>
+            </Title>
       <Container>
+        
 
       </Container>
     </Main>
--- src/components/todoList/TodoList2.style.js
+++ src/components/todoList/TodoList2.style.js
@@ -22,7 +22,46 @@ display: flex;
 border: 1px solid red;
 height: 390px;
 width: 1000px;
+`
+
+export const Input = styled.div`
+display:flex;
+width: 1000px;
+margin-bottom: 30px;
+
+input{
+    width: 100%;
+    border:1px solid #2c6beb;
+    margin-right: 30px;    
+    border-radius: 7px;
+    padding: 10px;
+    font-size:16px;
+
+    
+}
 
 
 
+
+button{
+    background-color: #FFF;
+    width: 120px;
+    padding:10px;
+    border-radius: 7px;
+    border: 1px solid #00ffff;
+    font-size: 16px;
+    color: #00ffff;
+    font-weight: 500;
+}
+`
+
+export const Title = styled.div`
+display: flex;
+justify-content: center;
+width: 1000px;
+border: 1px solid black;
+
+div{
+    flex:1;
+}
 `
