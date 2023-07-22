a272aebb246e9f6f12d26f96641694421f8f03e5
mike030188 <halol.imnida2023@gmail.com>
2023-07-22 11:21:47 +0500

    feat: Input_focus on Alert button using useRef

--- src/components/hook/useRef.js
+++ src/components/hook/useRef.js
@@ -1,16 +1,39 @@
 import {useRef} from 'react';
 
-export default function UseRefComponent () {
-    let ref = useRef(0);
-    
-    function handleClick() {
-        ref.current = ref.current + 1;
-        alert("Increament + 1")
-    }
 
+ /* alert button */ 
+
+// export default function UseRefComponent () {
+//     let ref = useRef(0);
+    
+//     function handleClick() {
+//         ref.current = ref.current + 1;
+//         alert("Increament + 1")
+//     }
+
+//     return (
+//         <>
+//             <button onClick={handleClick}>Alert Button</button>
+//         </>
+//     )
+// }
+
+
+/* Input(focus) on Alert Button */
+
+export default function () {
+    let inputRef = useRef(null);
+
+    function handleClick() {
+        inputRef.current.focus();
+    }
     return (
         <>
+            <input type="text" ref={inputRef}></input>
             <button onClick={handleClick}>Alert Button</button>
         </>
     )
 }
+
+
+
