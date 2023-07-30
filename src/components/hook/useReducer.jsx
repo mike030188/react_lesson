7a0351987635e1d08991ce7f6881c911f4256cd4
mike030188 <halol.imnida2023@gmail.com>
2023-07-30 16:26:48 +0500

    feat: Code updated

--- src/components/hook/useReducer.jsx
+++ src/components/hook/useReducer.jsx
@@ -3,9 +3,11 @@ import React, { useReducer } from 'react';
 const reducer = (state, action) => {
     switch (action.type) {
         case "INCREMENT":
-            return { number: state.number + 1, showText: state.showText }
+            return { number: state.number + 1, showText: state.showText, showText1: state.showText1 }
         case "setShowText":
             return { number: state.number, showText: !state.showText }
+        case "setShowText1":
+            return { number: state.number, showText1: !state.showText1 }
         default:
             return state;
     }
@@ -14,19 +16,25 @@ const reducer = (state, action) => {
 const UseReducerComp = () => {
     const [state, dispatch] = useReducer(reducer, { 
         number: 0, 
-        showText: false 
+        showText: true,
+        showText1: false,
+
     })
 
     return (
-    <div>
-        <h1>{state.number}</h1>
-        <button onClick={() => {
-            dispatch({ type: "INCREMENT" })
-            dispatch({ type: "setShowText" })
+    
+        <div>
+            <h1>{state.number}</h1>
+            <button onClick={() => {
+                dispatch({ type: "INCREMENT" })
+                dispatch({ type: "setShowText" })            
 
-        }}>Click Here</button>
-        {state.showText && <h1>Show Text</h1>}
-    </div>
+            }}>Click Here</button>
+            {state.showText ? 'Aka' : " Uka"}
+            
+        </div>       
+        
+    
   )
 }
 
